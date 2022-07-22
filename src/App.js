56681e749504cfa586c9289fccec0b5e7eb2f78c
yasminadelia <yasminadelia@yahoo.co.id>
2022-07-22 23:44:56 +0700

    chores: install tailwind for styling

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,11 +7,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className='md:text-xs xl:text-4xl '>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="App-link "
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
